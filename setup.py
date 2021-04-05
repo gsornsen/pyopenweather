9e70fb0bbe356b6bb8003b6e43cde7065df9e737
gsornsen <gsornsen@gmail.com>
2021-04-05 13:50:36 -0700

    Add classifiers/keywords for PyPi, fix issue finding packages

--- setup.py
+++ setup.py
@@ -14,12 +14,24 @@ setup(
     long_description=readme,
     long_description_content_type="text/markdown",
     url="https://github.com/gsornsen/pyopenweather",
-    packages=find_packages('weather'),
-    package_dir={"": "pyopenweather"},
+    packages=find_packages(),
+    # package_dir={"": "pyopenweather"},
     install_requires=[
         "requests-async>=0.6.2"
     ],
     python_requires=">=3.7",
+    classifiers=[
+      "License :: OSI Approved :: MIT License",
+      "Programming Language :: Python",
+      "Programming Language :: Python :: 3.7",
+      "Programming Language :: Python :: 3.8",
+      "Programming Language :: Python :: 3.9",
+      "Natural Language :: English",
+      "Operating System :: OS Independent",
+      "Development Status :: 5 - Production/Stable",
+      "Intended Audience :: Developers",
+      "Topic :: Software Development :: Libraries"],
+    keywords="openweathermap libary web api wrapper client wind temperature humidity",
     include_package_data=True,
     platforms="Posix; MacOS X; Windows"
 )
